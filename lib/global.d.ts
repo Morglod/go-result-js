38832498bbd6bb8c138a7486b0e317c9e1fcb29a
Кружков Владимир <morglod@gmail.com>
2018-06-29 00:10:02 +0300

    global Result type definition fix

--- lib/global.d.ts
+++ lib/global.d.ts
@@ -1,7 +1,8 @@
 export {};
-import { ResultA as ResultA_, ResultOk as ResultOk_, ResultErr as ResultErr_ } from './';
+import { ResultA as ResultA_, ResultOk as ResultOk_, ResultErr as ResultErr_, Result as Result_ } from './';
 declare global {
     const ResultA: typeof ResultA_;
     const ResultOk: typeof ResultOk_;
     const ResultErr: typeof ResultErr_;
+    type Result<T, ErrorT extends Error = Error> = Result_<T, ErrorT>;
 }
--- src/global.ts
+++ src/global.ts
@@ -4,6 +4,7 @@ import {
     ResultA as ResultA_,
     ResultOk as ResultOk_,
     ResultErr as ResultErr_,
+    Result as Result_,
     registerGlobally
 } from './';
 
@@ -11,6 +12,7 @@ declare global {
     const ResultA: typeof ResultA_;
     const ResultOk: typeof ResultOk_;
     const ResultErr: typeof ResultErr_;
+    type Result<T, ErrorT extends Error = Error> = Result_<T, ErrorT>;
 }
 
 registerGlobally();
